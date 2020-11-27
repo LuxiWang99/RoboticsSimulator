b23c5444e9fefbd6507059cab501419ea1e4dbf6
Luxi Wang <louis99@cs.washington.edu>
2020-11-26 23:41:05 -0800

    init

---
+++ Assets/Scripts/CarInit.cs
@@ -0,0 +1,11 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+public class CarInit : MonoBehaviour
+{
+    public GameObject myPrefab;
+    void Start()
+    {
+        Instantiate(myPrefab, new Vector3(0,0,0), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/floorControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floorControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject block;
+    void Start()
+    {
+         addDefaultObstacles();
+    }
+
+
+    void addObstacle(float x, float y)
+    {
+        Instantiate(block, new Vector3(x,0.0f,y), Quaternion.identity);
+    }
+
+
+    void addDefaultObstacles()
+    {
+        addObstacle(1.7f, 2.2f);
+        addObstacle(3.1f, 7.6f);
+        addObstacle(4.7f, 4.1f);
+    }
+    // int addLandmark();
+
+    // void addPost();
+
+    // void addGate();
+
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+}
