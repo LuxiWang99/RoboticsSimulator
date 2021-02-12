3af7fc2e7247cdba35de21cf8566aa70b4fe6992
Luxi Wang <louis99@cs.washington.edu>
2021-02-11 17:33:11 -0800

    Rover

--- Assets/Scripts/Rover.cs
+++ Assets/Scripts/Rover.cs
@@ -6,17 +6,20 @@ namespace DefaultNamespace
 {
     public class Rover : MonoBehaviour
     {
-        public GameObject robot;
         void Start()
         {
-            robot.transform.rotation = Random.rotation; // Randomly spin the rover to start
-            float x = 90;
-            if (Random.value < 0.5)
+            transform.rotation = Random.rotation; // Randomly spin the rover to start
+            transform.position = new Vector3(0, 0);
+        }
+        private void OnCollisionEnter(Collider other)
+        {
+            if (other.tag == "Obstacle")
             {
-                x = 0;
+                Debug.Log("");
             }
-
-            robot.transform.position = new Vector3(x, -3);
         }
     }
+    
+    
+
 }
