fd69c6facb61f2cf71d4a55037370898507389ac
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2021-02-11 18:06:44 -0800

    Add rover controls

---
+++ Assets/RoverController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Allows a user to control a rover using WASD controls.
+/// </summary>
+public class RoverController : MonoBehaviour
+{
+    public Rover rover;
+
+    private void Update()
+    {
+        float forwardBackward = Input.GetAxis("Vertical");
+        float leftRight = Input.GetAxis("Horizontal");
+        rover.setVelocity(forwardBackward, leftRight);
+    }
+}
--- Assets/Scripts/Rover.cs
+++ Assets/Scripts/Rover.cs
@@ -1,11 +1,21 @@
-using System;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-namespace DefaultNamespace
+/// <summary>
+/// Main component of the rover GameObject.
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class Rover : MonoBehaviour
 {
-    public class Rover : MonoBehaviour
+    public float linearSpeed;
+    public float rotationSpeed;
+
+    private Rigidbody rb;
+    private float driveforwardBackward;
+    private float driveLeftRight;
+
+    private void Start()
     {
+<<<<<<< Updated upstream
         void Start()
         {
             transform.rotation = Random.rotation; // Randomly spin the rover to start
@@ -23,3 +33,27 @@ namespace DefaultNamespace
     
 
 }
+=======
+        rb = GetComponent<Rigidbody>();
+        driveforwardBackward = 0.0f;
+        driveLeftRight = 0.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.forward * driveforwardBackward;
+        rb.angularVelocity = new Vector3(0.0f, driveLeftRight, 0.0f);
+    }
+
+    /// <summary>
+    /// Sets the velocity of the rover.
+    /// </summary>
+    /// <param name="forwardBackward">The linear velocity [-1.0, 1.0]. Positive values correspond to forward motion. Negative values correspond to backward motion.</param>
+    /// <param name="leftRight">The rotational velocity [-1.0, 1.0]. Positive values correspond to counterclockwise rotation. Negative values correspond to clockwise rotation.</param>
+    public void setVelocity(float forwardBackward, float leftRight)
+    {
+        this.driveforwardBackward = forwardBackward * linearSpeed;
+        this.driveLeftRight = leftRight * rotationSpeed;
+    }
+}
+>>>>>>> Stashed changes
