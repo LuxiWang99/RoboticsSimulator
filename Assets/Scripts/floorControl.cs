8d4d756fb89b9e3beb161cf3474eea81fe1ecd06
Luxi Wang <louis99@cs.washington.edu>
2021-02-13 12:55:29 -0800

    finished obstacle, working on post and gate

--- Assets/Scripts/floorControl.cs
+++ Assets/Scripts/floorControl.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class floorControl : MonoBehaviour
 {
+
+
+    public Rigidbody block;
+    public GameObject floor;
     public struct Leg
     {
         public Leg(int left, int right, (float, float, float) gps)
@@ -21,18 +25,20 @@ public class floorControl : MonoBehaviour
     private List<Leg> legs_ = new List<Leg>();
     private List<Vector3> tags = new List<Vector3>();
 
-    public GameObject Block;
     public GameObject Post;
     public GameObject Gate;
     void Start()
     {
-         addDefaultObstacles();
+         // addDefaultObstacles();
+         Instantiate(block, new Vector3(-4f, 4f, 4f), Quaternion.identity);
+         Instantiate(block, new Vector3(-5f, 5f, 5f), Quaternion.identity);
+         Instantiate(block, new Vector3(-1f, 1f, 1f), Quaternion.identity);
     }
 
 
     void addObstacle(float x, float y)
     {
-        Instantiate(Block, new Vector3(x,0.0f,y), Quaternion.identity);
+        Instantiate(block, new Vector3(x,0.0f,y), Quaternion.identity);
     }
 
 
@@ -76,28 +82,28 @@ public class floorControl : MonoBehaviour
         Instantiate(Gate, new Vector3(x,0.0f,y), Quaternion.identity);
      }
 
-     Transform readTrueTransform()
-     {
-         return 
-     }
+     // Transform readTrueTransform()
+     // {
+     //     
+     // }
 
 
-     List<Vector3> readLandmarks(Transform current_transform_truth) { 
-         List<Vector3> res = new List<Vector3>();
-        Vector3 robot_location = current_transform_truth.position;
-        foreach (Vector3 lm in tags) {
-            Vector3 reading = Vector3.Dot(current_transform_truth, lm);
-            float dist = (robot_location - lm).magnitude;
-            corrupt(reading, dist);
-            float t = obstacleIntersection(robot_location, lm, obstacles_);
-
-            if (dist > LANDMARK_MAX_RANGE || t < 0.999999) {
-                readoing *= 0;
-            }
-            res.Add(reading);
-        }
-        return res;
-     }
+     // List<Vector3> readLandmarks(Transform current_transform_truth) { 
+     //     List<Vector3> res = new List<Vector3>();
+     //    Vector3 robot_location = current_transform_truth.position;
+     //    foreach (Vector3 lm in tags) {
+     //        Vector3 reading = Vector3.Dot(current_transform_truth, lm);
+     //        float dist = (robot_location - lm).magnitude;
+     //        corrupt(reading, dist);
+     //        float t = obstacleIntersection(robot_location, lm, obstacles_);
+     //
+     //        if (dist > LANDMARK_MAX_RANGE || t < 0.999999) {
+     //            readoing *= 0;
+     //        }
+     //        res.Add(reading);
+     //    }
+     //    return res;
+     // }
 
      Leg getLeg(int index)
      {
