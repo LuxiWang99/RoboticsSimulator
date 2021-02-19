a470534bdcba64a86583f517638cedc3f48cedba
Nanolacker <connorreinholdtsen@outlook.com>
2021-02-18 16:58:42 -0800

    Make camera follow rover

---
+++ Assets/RoverCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RoverCamera : MonoBehaviour
+{
+    public GameObject rover;
+    public float height;
+
+    private void Update()
+    {
+        transform.position = rover.transform.position + new Vector3(0.0f, height, 0.0f);
+    }
+}
